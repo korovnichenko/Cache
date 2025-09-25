1597a82ea5334cec771664fe0d70143996a3b58d
Koros <korovnichenko.oa@phystech.edu>
2025-09-26 00:38:17 +0300

    added 2queue algorythm

--- headers/cache.hpp
+++ headers/cache.hpp
@@ -6,110 +6,131 @@
 
 template <typename T, typename KeyT>
 struct cache_t {
-    size_t sz_Am, sz_A1in;
+    size_t sz_Am, sz_A1in, sz_A1out;
     std::list<T> Am, A1in;
+    std::list<KeyT> A1out;
 
     using ListIt = typename std::list<T>::iterator;
-    std::unordered_map<KeyT, ListIt> hash_Am, hash_A1in;
+    std::unordered_map<KeyT, ListIt> hash_Am, hash_A1in, hash_A1out;
 
     cache_t(size_t Size) {
-        sz_Am = Size-Size/4;
-        sz_A1in = Size/4;
+        sz_A1in = Size / 4;
+        sz_Am = Size - sz_A1in;
+        sz_A1out = Size / 2;
     }
 
     bool full_Am(){
-        return Am.size() == sz_Am;
+        return Am.size() == sz_Am; //+
     }
     bool full_A1in(){
-        return A1in.size() == sz_A1in;
+        return A1in.size() == sz_A1in; //+
     }
-    void Amprint(){
+    bool full_A1out(){
+        return A1out.size() == sz_A1out; //+
+    }
+    void Amprint(){ //+
         if(!(Am.empty())){
-            std::cout<<": ";
+            std::cout<<":    ";
             for(auto elem: Am)
                 std::cout<<elem<<" ";
             std::cout<<"\n";
         }
         else
-            std::cout<<" list is empty\n ";
+            std::cout<<"     is empty\n";
     }
-    void A1inprint(){
-        if(!(A1in.empty())){
+        void A1outprint(){ //+
+        if(!(A1out.empty())){
             std::cout<<": ";
-            for(auto elem: A1in)
+            for(auto elem: A1out)
                 std::cout<<elem<<" ";
             std::cout<<"\n";
         }
         else
-            std::cout<<" list is empty\n ";
+        std::cout<<"  is empty\n";
     }
-
-    // bool lookup_update(KeyT key){
-    //     auto hit = hash_Am.find(key);
-    //     if (hit == hash_.end()) {
-    //         std::cout<<" - no hit | ";
-    //         if (full_cache()) {
-    //             hash_.erase((KeyT)Am.back());
-    //             Am.pop_back();
-    //         }
-    //         Am.push_front(key);
-    //         hash_[key] = Am.begin();
-    //         return false;
-    //     }
-    //     std::cout<<" - hit | ";
-    //     auto eltit = hit->second;
-        
-    //     if (eltit != Am.begin())
-    //     Am.splice(A.begin(), Am, eltit, std::next(eltit));
-    //     return true;
-    //}
-    // template <typename F>
-    // bool slowq_keyget(KeyT key, F slow_get_page){
-    //     auto hit = hash_.find(key);
-    //     if (hit == hash_.end()) {
-    //         std::cout<<"added to slow queue";
-    //         if (full_A1in()) {
-    //             hash_.erase((KeyT)A1in.back());
-    //             A1in.pop_back();
-    //         }
-    //         A1in.push_front(slow_get_page(key));
-    //         hash_[key] = A1in.begin();
-    //         return false;
-    //     }
-        
-    //     std::cout<<"found in slow queue\n";
-    //     auto eltit = hit->second;
-    //     Am.push_front(key);m
-    //     //hash_.erase(hit);
-    //     A1in.erase(eltit);
-    //     return true;
-    // }
+    void A1inprint(){ //+
+        if(!(A1in.empty())){
+            std::cout<<":  ";
+            for(auto elem: A1in)
+                std::cout<<elem<<" ";
+                std::cout<<"\n";
+        }
+        else
+        std::cout<<" is empty\n";
+    }
     template< typename F>
     bool Q2(KeyT key, F slow_get_page){
+        //met in Am
         auto hit = hash_Am.find(key);
-        if(hit == hash_Am.end()){
-            hit = hash_A1in.find(key);
-            if(hit == hash_A1in.end()){
-                std::cout<<"added to slow queue\n";
-                if (full_A1in()) {
-                    hash_A1in.erase((KeyT)A1in.back());
-                    A1in.pop_back();
-                    std::cout<<"erase нахуй \n";
-                }
-                A1in.push_front(slow_get_page(key));
-                hash_A1in[key] = A1in.begin();
-                return false;
-            }
+        if(hit != hash_Am.end()){
+            std::cout<<" - hit | \n";
             auto eltit = hit->second;
-            if (eltit != A1in.begin())
-                A1in.splice(A1in.begin(), A1in, eltit, std::next(eltit));
+            if (eltit != Am.begin())
+                Am.splice(Am.begin(), Am, eltit, std::next(eltit));
             return true;
         }
-        std::cout<<" - hit | ";
-        auto eltit = hit->second;
-        if (eltit != Am.begin())
-        Am.splice(Am.begin(), Am, eltit, std::next(eltit));
-        return true;
+        //met in A1out
+        hit = hash_A1out.find(key);
+        if(hit != hash_A1out.end()){
+            //exchange elements between Am and A1out
+            if(full_Am()){
+
+                //getting key from Am and deleting it afterwards
+                KeyT last_elem = Am.back();
+                hash_Am.erase(last_elem);
+                Am.pop_back();
+
+                //adding new element with to the Am
+                Am.push_front(key);
+                hash_Am[key] = Am.begin();
+
+                //swapping met key in A1out with one found earlier
+                hash_A1out.erase(key);
+                A1out.erase(hit->second);
+                A1out.push_front(last_elem);
+                hash_A1out[last_elem] = A1out.begin();
+                return false;
+            }
+            Am.push_front(key);
+            hash_Am[key] = Am.begin();
+            A1out.erase(hit->second);
+            hash_A1out.erase(key);
+
+            return false;
+        }
+
+        //met in a1in
+        hit = hash_A1in.find(key);
+        if(hit != hash_A1in.end())
+            return true;
+        if (full_A1in()) {
+       
+            //element from A1in goes to A1out
+            KeyT last_in = A1in.back();
+            if (full_A1out()) {
+                //deleting last element from A1out
+                KeyT last_out = A1out.back();
+                hash_A1out.erase(last_out);
+                A1out.pop_back();
+            }
+            
+            //erasing last from A1in
+            hash_A1in.erase(last_in); 
+            A1in.pop_back();
+
+            //adding that element to A1out
+            A1out.push_front(last_in);    
+            hash_A1out[last_in] = A1out.begin();
+            std::cout<<"added to warm queue\n";
+
+        }
+
+        //just adding new element to A1in
+        std::cout<<" added to cold queue\n";
+        A1in.push_front(slow_get_page(key));
+        hash_A1in[key] = A1in.begin();
+        std::cout<<"-----MISS-----\n";
+        return false;
     }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -11,18 +11,23 @@ int main() {
     int Size;
     std::cout<<"Choose size of cache: ";
     std::cin>>Size;
+    int i = 1, hit = 0;
     cache_t<int, int> cache(Size);
-    int data[] = {1, 2, 3, 4, 1, 2, 5, 1, 2, 4, 3, 4};
+    int data[] = { 2, 6, 1, 2, 1, 2, 1, 2};
     for(int number: data){
+        std::cout<<"\n==============\n"<<i<<" iteration"<<"\n==============\n";
         std::cout<<number;
         // cache.slowq_keyget<typeof slow_get_page_int>(number, slow_get_page_int);
         // cache.lookup_update(number);
-        cache.Q2(number, slow_get_page_int);
+        hit += cache.Q2(number, slow_get_page_int);
         std::cout<<"A1in";
         cache.A1inprint();
-        std::cout<<"cache";
+        std::cout<<"A1out";
+        cache.A1outprint();  
+        std::cout<<"Am";
         cache.Amprint();
-        
+        i++;
     }
+    std::cout<<"hit = "<<hit;
     return 0;
 }
